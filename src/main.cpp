0eeae079383a8aa99bacaa847dce588955007233
Neel Raiyani <nraiyani+github@gmail.com>
2021-08-11 21:06:40 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <fmt/core.h>
+#include <cppitertools/range.hpp>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    fmt::print("C++ Template using CMake Presets\n");
+
+    for (auto &&i : iter::range(10))
+    {
+        fmt::print("{}\n", i);
+    }
+}
